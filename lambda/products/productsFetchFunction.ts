74ee73e5a50a1725c916b50ff00f6c17237c68c6
Lucas Calebe <lucascalebe07@hotmail.com>
2024-09-12 11:40:03 -0300

    ✨ creating first code of lambda function to find products

---
+++ lambda/products/productsFetchFunction.ts
@@ -0,0 +1,21 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda";
+
+export async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
+    const httpMethod = event.httpMethod;
+
+    if (event.resource === "/products") {
+        if (httpMethod === "GET") {
+            console.log("GET /products");
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify({ message: "GET Products - OK" })
+            }
+        }
+    }
+
+    return {
+        statusCode: 400,
+        body: JSON.stringify({ message: "Bad Request" })
+    }
+}
