a77437c4116d51d76c21df680466911a536aa263
Lucas Calebe <lucascalebe07@hotmail.com>
2024-09-13 15:16:09 -0300

    ✨ implementing search of products and product by id in dynamo repository

--- lambda/products/layers/productsLayer/nodejs/node20/productRepository.ts
+++ lambda/products/layers/productsLayer/nodejs/node20/productRepository.ts
@@ -2,7 +2,7 @@ import { DocumentClient } from "aws-sdk/clients/dynamodb"
 import { v4 as uuid } from "uuid"
 
 export interface Product {
-    id: number;
+    id: string;
     productName: string;
     price: number;
     model: string;
@@ -15,5 +15,28 @@ export class ProductRepository {
     constructor(dynamoClient: DocumentClient, productsTable: string) {
         this.dynamoClient = dynamoClient
         this.productsTable = productsTable
+    }
+
+    async findAllProducts(): Promise<Product[]> {
+        const data = await this.dynamoClient.scan({
+            TableName: this.productsTable
+        }).promise()
+
+        return data.Items as Product[]
+    }
+
+    async findProductById(id: number): Promise<Product> {
+        const data = await this.dynamoClient.get({
+            TableName: this.productsTable,
+            Key: {
+                id
+            }
+        }).promise()
+
+        if (data.Item) {
+            return data.Item as Product
+        } else {
+            throw new Error(`Product with id ${id} not found`)
+        }
     }
 }
