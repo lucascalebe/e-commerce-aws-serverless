f986b085500892876dc174fa6d968dbd3b7be69c
Lucas Calebe <lucascalebe07@hotmail.com>
2024-09-13 11:51:17 -0300

    ✨ creating dynamodb table with cdk

--- lib/productsApp-stack.ts
+++ lib/productsApp-stack.ts
@@ -1,13 +1,24 @@
 import * as lambda from "aws-cdk-lib/aws-lambda"
 import * as lambdaNodeJS from "aws-cdk-lib/aws-lambda-nodejs"
 import * as cdk from "aws-cdk-lib"
+import * as dynadb from "aws-cdk-lib/aws-dynamodb"
 import { Construct } from "constructs"
 
 export class ProductsAppStack extends cdk.Stack {
     readonly productsFetchHandler: lambdaNodeJS.NodejsFunction
+    readonly productsTable: dynadb.Table
 
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props)
+
+        this.productsTable = new dynadb.Table(this, "Products", {
+            tableName: "Products",
+            partitionKey: { name: "id", type: dynadb.AttributeType.STRING },
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            billingMode: dynadb.BillingMode.PROVISIONED,
+            readCapacity: 1,
+            writeCapacity: 1,
+        })
 
         this.productsFetchHandler = new lambdaNodeJS.NodejsFunction(this, "ProductsFetchFunction", {
             runtime: lambda.Runtime.NODEJS_20_X,
