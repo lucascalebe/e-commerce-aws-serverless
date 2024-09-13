53e39636e0aa9792a99125c5a34dd87dea21d6ef
Lucas Calebe <lucascalebe07@hotmail.com>
2024-09-13 12:10:52 -0300

    ✨ Adding endpoints of POST, PUT, DELETE in gateway and triggering lambda stack

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -5,7 +5,8 @@ import * as cdk from "aws-cdk-lib"
 import { Construct } from "constructs"
 
 interface ECommerceApiStackProps extends cdk.StackProps {
-    productsFetchHandler: lambdaNodeJS.NodejsFunction
+    productsFetchHandler: lambdaNodeJS.NodejsFunction,
+    productsAdminHandler: lambdaNodeJS.NodejsFunction
 }
 
 export class ECommerceApiStack extends cdk.Stack {
@@ -34,9 +35,23 @@ export class ECommerceApiStack extends cdk.Stack {
         })
 
         const productsFetchIntegration = new apigateway.LambdaIntegration(props.productsFetchHandler)
-
-        // "/products"
+        // GET /products
         const productsResource = api.root.addResource("products")
         productsResource.addMethod("GET", productsFetchIntegration)
+
+        // GET /products/{id}
+        const productIdResource = productsResource.addResource("{id}")
+        productIdResource.addMethod("GET", productsFetchIntegration)
+
+        
+        const productsAdminIntegration = new apigateway.LambdaIntegration(props.productsAdminHandler)
+         // POST/products
+         productsResource.addMethod("POST", productsAdminIntegration)
+
+         // PUT /products/{id}
+         productIdResource.addMethod("PUT", productsAdminIntegration)
+
+         // DELETE /products/{id}
+         productIdResource.addMethod("DELETE", productsAdminIntegration)
     }
 }
