7fa26ea2d9048fdacb789b52f7f241f6b688e1bf
Lucas Calebe <lucascalebe07@hotmail.com>
2024-09-13 14:50:54 -0300

    ✨ creating products repository

--- lambda/products/layers/productsLayer/nodejs/node20/productRepository.ts
+++ lambda/products/layers/productsLayer/nodejs/node20/productRepository.ts
@@ -1,7 +1,19 @@
+import { DocumentClient } from "aws-sdk/clients/dynamodb"
+import { v4 as uuid } from "uuid"
 
 export interface Product {
     id: number;
     productName: string;
     price: number;
     model: string;
+}
+
+export class ProductRepository {
+    private dynamoClient: DocumentClient
+    private productsTable: string
+
+    constructor(dynamoClient: DocumentClient, productsTable: string) {
+        this.dynamoClient = dynamoClient
+        this.productsTable = productsTable
+    }
 }
