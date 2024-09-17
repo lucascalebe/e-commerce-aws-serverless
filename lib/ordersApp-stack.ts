4981b8a2ccdbe9ee860ce8430c10e72f28089f77
Lucas Calebe <lucascalebe07@hotmail.com>
2024-09-17 14:22:10 -0300

    ✨ Creating function of orders

--- lib/ordersApp-stack.ts
+++ lib/ordersApp-stack.ts
@@ -10,6 +10,8 @@ interface OrdersAppStackProps extends cdk.StackProps {
 }
 
 export class OrdersAppStack extends cdk.Stack {
+    readonly ordersHandler: lambdaNodeJS.NodejsFunction
+
     constructor(scope: Construct, id: string, props: OrdersAppStackProps) {
         super(scope, id, props)
 
@@ -31,5 +33,28 @@ export class OrdersAppStack extends cdk.Stack {
         const productsLayerArn = ssm.StringParameter.valueForStringParameter(this, "ProductsLayerVersionArn")
         const productsLayer = lambda.LayerVersion.fromLayerVersionArn(this, "ProductsLayerVersionArn", productsLayerArn)
 
+        this.ordersHandler = new lambdaNodeJS.NodejsFunction(this, "OrdersFunction", {
+            runtime: lambda.Runtime.NODEJS_20_X,
+            functionName: "OrdersFunction",
+            entry: "lambda/orders/ordersFunction.ts",
+            handler: "handler",
+            memorySize: 512,
+            timeout: cdk.Duration.seconds(2),
+            bundling: {
+                minify: true,
+                sourceMap: false
+            },
+            description: "Events related to system",
+            environment: {
+                PRODUCTS_TABLE: props.productsDynamoTable.tableName,
+                ORDERS_TABLE: ordersTable.tableName 
+            },
+            layers: [ordersLayer, productsLayer],
+            tracing: lambda.Tracing.ACTIVE,
+            insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_317_0
+        })
+
+        ordersTable.grantReadWriteData(this.ordersHandler)
+        props.productsDynamoTable.grantReadData(this.ordersHandler)
     }
 }
